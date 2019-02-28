ec7373fecaf7a7c3701ee9f9968a5985db5dfdc5
Lucas Félix <lucasgsfelix@gmail.com>
2019-02-28 12:15:11 -0300

    Auto stash before rebase of "origin/Setup__and_Tox"

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+"""Tests from of_stats napp."""
+
+>>>>>>> Stashed changes
 import sys
 import os
 from pathlib import Path
@@ -7,8 +12,14 @@ if 'VIRTUAL_ENV' in os.environ:
 else:
     BASE_ENV = Path('/')
 
+<<<<<<< Updated upstream
 #STATUS_NAPP_PATH = BASE_ENV / '/var/lib/kytos/napps/..'
 
 STATUS_NAPP_PATH = str(BASE_ENV) + '/var/lib/kytos/napps/kytos/'
 
 sys.path.insert(0, STATUS_NAPP_PATH)
+=======
+OF_STATS_PATH = BASE_ENV / '/var/lib/kytos/napps/..'
+
+sys.path.insert(0, str(OF_STATS_PATH))
+>>>>>>> Stashed changes
